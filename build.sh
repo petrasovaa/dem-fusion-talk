7f65433a48d14239d5d1c0e2dbd45ce2c867e32b
Anna Petrasova <kratochanna@gmail.com>
2017-02-24 00:11:58 -0500

    initial commit

---
+++ build.sh
@@ -0,0 +1,5 @@
+./copy-common-files.py --dst-dir=build
+
+./build-slides.py --outdir=build --title="UAV DEM fusion" \
+    --meta-description="NCGIS 2017 presentation" --meta-author="Anna Petrasova"\
+    presentation.html
